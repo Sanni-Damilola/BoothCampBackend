a210f1a56273aa615b1c21509057cc1503183812
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-03 22:08:07 +0100

    boothCamp

---
+++ Interface/interface.ts
@@ -0,0 +1,15 @@
+export interface Device {
+  ticketNumber: string;
+  lostCard: boolean;
+  deviceType: string;
+  numberOfDeviceBrought: string;
+  pORb: string;
+  collected: boolean;
+  userID: string;
+}
+
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+}
